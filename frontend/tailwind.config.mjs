e7f3b8d5ebfdcc75b53619b9caa9712d4f5342ba
jaswantk19 <jaswantkondurofficial@gmail.com>
2024-11-09 02:41:07 +0530

    feat(initial-setup): created landing page for portfolio

---
+++ frontend/tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Red Hat Display Variable', ...defaultTheme.fontFamily.sans],
+			  },
+		},
+
+	},
+	plugins: [],
+}
